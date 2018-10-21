719110d8d28bc092e57d5d4d0877125daeccab3b
Julian Büning <julian.buening@rwth-aachen.de>
2018-10-21 15:52:42 +0200

    LiveRegister: remove metatdata annotation for debugging

--- lib/Core/InfiniteLoopDetectionFlags.h
+++ lib/Core/InfiniteLoopDetectionFlags.h
@@ -20,8 +20,6 @@ extern llvm::cl::opt<bool> InfiniteLoopDetectionDisableTwoPredecessorOpt;
 
 extern llvm::cl::opt<bool> InfiniteLoopDetectionDisableLiveVariableAnalysis;
 
-extern llvm::cl::opt<bool> InfiniteLoopDetectionDebugLiveVariableAnalysis;
-
 extern llvm::cl::opt<bool> InfiniteLoopLogStateJSON;
 
 #ifdef HAVE_ZLIB_H
