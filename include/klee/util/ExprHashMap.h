c58f069cf45b480b412dc3f8fdee83fb8c8e2aea
Julian Büning <julian.buening@rwth-aachen.de>
2018-11-29 18:33:15 +0100

    modernize ExprHashMap.h

--- include/klee/util/ExprHashMap.h
+++ include/klee/util/ExprHashMap.h
@@ -12,51 +12,30 @@
 
 #include "klee/Expr.h"
 
-#include <ciso646>
-#ifdef _LIBCPP_VERSION
 #include <unordered_map>
 #include <unordered_set>
-#define unordered_map std::unordered_map
-#define unordered_set std::unordered_set
-#else
-#include <tr1/unordered_map>
-#include <tr1/unordered_set>
-#define unordered_map std::tr1::unordered_map
-#define unordered_set std::tr1::unordered_set
-#endif
 
 namespace klee {
 
-  namespace util {
-    struct ExprHash  {
-      unsigned operator()(const ref<Expr> e) const {
-        return e->hash();
-      }
-    };
-    
-    struct ExprCmp {
-      bool operator()(const ref<Expr> &a, const ref<Expr> &b) const {
-        return a==b;
-      }
-    };
+namespace util {
+struct ExprHash {
+  unsigned operator()(const ref<Expr> e) const { return e->hash(); }
+};
+
+struct ExprCmp {
+  bool operator()(const ref<Expr> &a, const ref<Expr> &b) const {
+    return a == b;
   }
-  
-  template<class T> 
-  class ExprHashMap : 
+};
+} // namespace util
 
-    public unordered_map<ref<Expr>,
-                         T,
-                         klee::util::ExprHash,
-                         klee::util::ExprCmp> {
-  };
-  
-  typedef unordered_set<ref<Expr>,
-                        klee::util::ExprHash,
-                        klee::util::ExprCmp> ExprHashSet;
+template <typename T>
+using ExprHashMap =
+    std::unordered_map<ref<Expr>, T, util::ExprHash, util::ExprCmp>;
 
-}
+using ExprHashSet =
+    std::unordered_set<ref<Expr>, util::ExprHash, util::ExprCmp>;
 
-#undef unordered_map
-#undef unordered_set
+} // namespace klee
 
 #endif
