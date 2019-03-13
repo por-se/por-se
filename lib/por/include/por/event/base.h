bd0341d11601263d6b3cce550b679bdc05b656a7
Julian Büning <julian.buening@rwth-aachen.de>
2019-03-13 20:10:25 +0100

    base: make comparison operator const

--- lib/por/include/por/event/base.h
+++ lib/por/include/por/event/base.h
@@ -111,7 +111,7 @@ namespace por::event {
 		virtual util::iterator_range<std::shared_ptr<event>*> predecessors() = 0;
 		virtual util::iterator_range<std::shared_ptr<event> const*> predecessors() const = 0;
 
-		bool operator<(event const& rhs) {
+		bool operator<(event const& rhs) const {
 			if(rhs._tid == _tid) {
 				return _depth < rhs._depth;
 			} else {
