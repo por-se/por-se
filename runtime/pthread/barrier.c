8a81dfd072296a7b0117ec9128c20eac8f184d6b
David Laprell <david.laprell@rwth-aachen.de>
2019-04-09 00:19:35 +0200

    pthread runtime: fix wrong return value for barrier_wait + comments

--- runtime/pthread/barrier.c
+++ runtime/pthread/barrier.c
@@ -38,24 +38,36 @@ int pthread_barrier_destroy(pthread_barrier_t *barrier) {
 }
 
 int pthread_barrier_wait(pthread_barrier_t *barrier) {
+  // We disable the thread scheduling here to not let the data race detection error
+  // before we actually can acquire the mutex.
+  // If the object is invalid the state is terminated in any case.
+  // But in the case of a correct object the call to `pthread_mutex_lock` will reenable thread scheduling
+  klee_toggle_thread_scheduling(0);
+
   if (barrier->count == 0) {
     klee_report_error(__FILE__, __LINE__, "Use of uninitialized/destroyed barrier", "user");
   }
 
-  klee_toggle_thread_scheduling(0);
   kpr_check_if_valid(pthread_barrier_t, barrier);
 
   pthread_mutex_lock(&barrier->mutex);
 
   barrier->currentCount++;
 
+  int ret = 0;
+
   if (barrier->currentCount == barrier->count) {
     pthread_cond_broadcast(&barrier->cond);
     barrier->currentCount = 0;
+
+    // Should only be returned to one thread (unspecified which one)
+    // TODO: Maybe choice of thread can be made symbolic in the future?
+    ret = PTHREAD_BARRIER_SERIAL_THREAD;
   } else {
     pthread_cond_wait(&barrier->cond, &barrier->mutex);
   }
 
   pthread_mutex_unlock(&barrier->mutex);
-  return 0;
+
+  return ret;
 }
