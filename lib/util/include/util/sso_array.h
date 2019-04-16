9cbd591c3e22e0a8e4a5a7a49453650ce10a2dcc
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2019-04-16 11:05:23 +0200

    fancyness of sso_array (correctness for uninitialized case and move/cpy constructors)

--- lib/util/include/util/sso_array.h
+++ lib/util/include/util/sso_array.h
@@ -3,6 +3,7 @@
 #include <util/create_uninitialized.h>
 
 #include <cstddef>
+#include <cstdlib>
 #include <type_traits>
 #include <algorithm>
 
@@ -30,44 +31,99 @@ namespace util {
 	public:
 		sso_array() noexcept = default;
 
-		sso_array(std::size_t const size)
+		// WARNING when using this ctor directly:
+		// All elements *MUST* be initialized before the sso_array is destructed!
+		sso_array(create_uninitialized_t, std::size_t const size) noexcept
 			: _size(size)
 		{
+			assert((_size * sizeof(T)) / sizeof(T) == _size && "overflow in size computation");
 			if constexpr(sso_capacity == 0) {
-				reinterpret_cast<T*&>(data) = new T[_size]();
+				if(_size > 0) {
+					reinterpret_cast<T*&>(_data) = static_cast<T*>(std::malloc(_size * sizeof(T)));
+					assert(reinterpret_cast<T*&>(_data) && "malloc failed");
+				}
 			} else if(_size > sso_capacity) {
-				reinterpret_cast<T*&>(data) = new T[_size]();
+				reinterpret_cast<T*&>(_data) = static_cast<T*>(std::malloc(_size * sizeof(T)));
+				assert(reinterpret_cast<T*&>(_data) && "malloc failed");
+			}
+		}
+
+		sso_array(std::size_t const size) noexcept(std::is_nothrow_default_constructible_v<T>)
+			: sso_array(create_uninitialized, size)
+		{
+			if constexpr(sso_capacity == 0) {
+				for(std::size_t i = 0; i < _size; ++i) {
+					new(reinterpret_cast<T*&>(_data) + i) T();
+				}
+			} else if(_size > sso_capacity) {
+				for(std::size_t i = 0; i < _size; ++i) {
+					new(reinterpret_cast<T*&>(_data) + i) T();
+				}
 			} else {
 				for(std::size_t i = 0; i < _size; ++i) {
-					new(reinterpret_cast<T(&)[sso_capacity]>(data) + i) T();
+					new(reinterpret_cast<T(&)[sso_capacity]>(_data) + i) T();
 				}
 			}
 		}
 
-		sso_array(create_uninitialized_t, std::size_t const size)
-			: _size(size)
+		sso_array(sso_array const& rhs) noexcept(std::is_nothrow_copy_constructible_v<T>)
+			: sso_array(create_uninitialized, rhs.size)
 		{
 			if constexpr(sso_capacity == 0) {
-				reinterpret_cast<T*&>(_data) = new T[_size];
+				for(std::size_t i = 0; i < _size; ++i) {
+					new(reinterpret_cast<T*&>(_data) + i) T(rhs[i]);
+				}
 			} else if(_size > sso_capacity) {
-				reinterpret_cast<T*&>(_data) = new T[_size];
+				for(std::size_t i = 0; i < _size; ++i) {
+					new(reinterpret_cast<T*&>(_data) + i) T(rhs[i]);
+				}
 			} else {
 				for(std::size_t i = 0; i < _size; ++i) {
-					new(reinterpret_cast<T(&)[sso_capacity]>(_data) + i) T;
+					new(reinterpret_cast<T(&)[sso_capacity]>(_data) + i) T(rhs[i]);
 				}
 			}
 		}
-
-		sso_array(sso_array const&) = delete;
 		sso_array& operator=(sso_array const&) = delete;
-		sso_array(sso_array&&) = delete;
+
+		sso_array(sso_array&& rhs) noexcept(std::is_nothrow_move_constructible_v<T>)
+			: _size(rhs.size)
+		{
+			assert((_size * sizeof(T)) / sizeof(T) == _size && "overflow in size computation");
+			if constexpr(sso_capacity == 0) {
+				reinterpret_cast<T*&>(_data) = reinterpret_cast<T*&>(rhs._data);
+				rhs._size = 0;
+				#if DEBUG
+					reinterpret_cast<T*&>(rhs._data) = nullptr;
+				#endif
+			} else if(_size > sso_capacity) {
+				reinterpret_cast<T*&>(_data) = reinterpret_cast<T*&>(rhs._data);
+				rhs._size = 0;
+				#if DEBUG
+					reinterpret_cast<T*&>(rhs._data) = nullptr;
+				#endif
+			} else {
+				auto& lhs_data = reinterpret_cast<T(&)[sso_capacity]>(_data);
+				auto& rhs_data = reinterpret_cast<T(&)[sso_capacity]>(rhs._data);
+				for(std::size_t i = 0; i < _size; ++i) {
+					new(lhs_data + i) T(std::move(rhs_data + i));
+				}
+			}
+		}
 		sso_array& operator=(sso_array&&) = delete;
 
 		~sso_array() {
 			if constexpr(sso_capacity == 0) {
-				delete[] reinterpret_cast<T*&>(_data);
+				if(_size > 0) {
+					for(std::size_t i = 0; i < _size; ++i) {
+						reinterpret_cast<T*&>(_data)[i].~T();
+					}
+					std::free(reinterpret_cast<T*&>(_data));
+				}
 			} else if(_size > sso_capacity) {
-				delete[] reinterpret_cast<T*&>(_data);
+					for(std::size_t i = 0; i < _size; ++i) {
+						reinterpret_cast<T*&>(_data)[i].~T();
+					}
+					std::free(reinterpret_cast<T*&>(_data));
 			} else {
 				for(std::size_t i = 0; i < _size; ++i) {
 					reinterpret_cast<T(&)[sso_capacity]>(_data)[i].~T();
