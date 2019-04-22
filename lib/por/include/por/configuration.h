69014abd3db45f15bc382177fa66836ffdf27f1f
Julian Büning <julian.buening@rwth-aachen.de>
2019-04-22 23:55:47 +0200

    configuration: attach_execution_state(*s) -> standby_execution_state(*s)

--- lib/por/include/por/configuration.h
+++ lib/por/include/por/configuration.h
@@ -71,9 +71,9 @@ namespace por {
 		auto const& cond_heads() const noexcept { return _cond_heads; }
 
 		auto const& schedule() const noexcept { return _schedule; }
-		klee::ExecutionState const* standby_execution_state() const noexcept { return _standby_states.back(); }
 
-		void attach_execution_state(klee::ExecutionState const* s) {
+		klee::ExecutionState const* standby_execution_state() const noexcept { return _standby_states.back(); }
+		void standby_execution_state(klee::ExecutionState const* s) {
 			assert(s != nullptr);
 			assert(_schedule.size() > 0);
 			assert(_schedule_pos > 0);
