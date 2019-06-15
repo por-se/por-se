c712d9f27f66f8cb3233851d70870801e24fffc3
Julian Büning <julian.buening@rwth-aachen.de>
2019-06-15 14:36:10 +0200

    test/Runtime/pthread/regression/unjoined.c

---
+++ test/Runtime/POSIX/pthread/regression/unjoined.c
@@ -0,0 +1,44 @@
+// RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
+// RUN: %clang %s -emit-llvm %O0opt -g -c -DLOCK_NOTHING -o %t-lock.bc
+// RUN: %clang %s -emit-llvm %O0opt -g -c -DPTHREAD_EXIT_FROM_MAIN -o %t-exit.bc
+// RUN: rm -rf %t.klee-out
+// RUN: rm -rf %t-lock.klee-out
+// RUN: rm -rf %t-exit.klee-out
+// RUN: %klee --output-dir=%t.klee-out --posix-runtime --exit-on-error %t.bc
+// RUN: %klee --output-dir=%t-lock.klee-out --posix-runtime --exit-on-error %t-lock.bc 2>&1 | FileCheck --check-prefix=CHECK-LOCK %s
+// RUN: %klee --output-dir=%t-exit.klee-out --posix-runtime --exit-on-error-type=Deadlock %t-exit.bc
+// RUN: test -f %t.klee-out/test000001.deadlock.err
+
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_THREADS 2
+
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+void *thread_routine(void *arg) {
+#ifdef LOCK_NOTHING
+  pthread_mutex_lock(&m);
+#endif
+  puts("doing nothing, really.");
+#ifdef LOCK_NOTHING
+  pthread_mutex_unlock(&m);
+#endif
+  pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t threads[NUM_THREADS];
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    pthread_create(&threads[i], NULL, thread_routine, (void *)(size_t)i);
+  }
+
+  puts("exiting without waiting for any other threads");
+#ifdef PTHREAD_EXIT_FROM_MAIN
+  pthread_exit(0);
+#else
+  return 0;
+#endif
+}
+
+// CHECK-LOCK: KLEE: done: completed paths = 2
