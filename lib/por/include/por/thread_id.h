522ba7750eee5143f54bb2f1e781cd2c03c56795
Julian Büning <julian.buening@rwth-aachen.de>
2019-09-16 12:38:53 +0200

    thread_id: fix and refactor
    fixes include:
    resize() handles all cases
    operator<() corrected

--- lib/por/include/por/thread_id.h
+++ lib/por/include/por/thread_id.h
@@ -21,136 +21,169 @@ namespace por {
 		} _data;
 
 		void resize(std::size_t new_size) {
-			if(_size == new_size)
+			if(_size == new_size) {
 				return;
+			}
 
 			std::uint16_t* p = new_size > asize ? new std::uint16_t[new_size] : _data.a;
+			std::uint16_t* q = data();
 
-			if(p != data()) {
+			if(p != q) {
 				for(std::size_t i = 0; i < new_size && i < _size; ++i) {
-					p[i] = data()[i];
+					p[i] = q[i];
 				}
 			}
 
-			if(_size > asize) delete[] _data.p;
+			if(_size > asize) {
+				delete[] q;
+			}
 
 			_size = new_size;
+			if(new_size > asize) {
+				_data.p = p;
+			}
+		}
 
-			if(new_size > asize) _data.p = p;
+		void clear() noexcept {
+			if(_size > asize) {
+				delete[] _data.p;
+			}
+			_size = 0;
 		}
 
 		std::uint16_t* data() noexcept { return _size <= asize ? _data.a : _data.p; }
+		std::uint16_t const* data() const noexcept { return _size <= asize ? _data.a : _data.p; }
 
 	public:
-		thread_id() noexcept : _size(0) {
-			_data.p = nullptr;
-		}
+		thread_id() noexcept : _size(0) { }
 
-		thread_id(const thread_id& parent, std::uint16_t localId) : _size(parent._size + 1) {
+		thread_id(thread_id const& parent, std::uint16_t localId) : thread_id() {
 			assert(localId != 0 && "Local ids must be non-zero");
-			_data.p = nullptr;
 
-			std::uint16_t* p = _size > asize ? (_data.p = new std::uint16_t[_size]) : _data.a;
-			std::memcpy(p, parent.ids(), (_size - 1) * sizeof(*p));
-
+			resize(parent._size + 1);
+			std::memcpy(data(), parent.data(), parent._size * sizeof(*data()));
 			data()[_size - 1] = localId;
 		}
 
-		explicit thread_id(std::uint16_t localId)
-			: _size(1) {
-			assert(localId != 0 && "Local ids must be non-zero");
-			_data.p = nullptr;
+		explicit thread_id(std::uint16_t localId) : thread_id(thread_id(), localId) { }
 
-			data()[0] = localId;
+		thread_id(thread_id const& other) : thread_id() {
+			resize(other._size);
+			std::memcpy(data(), other.data(), other._size * sizeof(*data()));
 		}
 
-		thread_id(thread_id const& other) : _size(other._size) {
-			if(_size > asize) {
-				_data.p = new std::uint16_t[_size];
-			} else {
-				_data.p = nullptr;
+		thread_id& operator=(thread_id const& other) noexcept {
+			if(this != &other) {
+				resize(other._size);
+				std::memcpy(data(), other.data(), other._size * sizeof(*data()));
 			}
-
-			const void* from = _size <= asize ? other._data.a : other._data.p;
-			std::memcpy(data(), from, _size * sizeof(*data()));
+			return *this;
 		}
 
 		thread_id(thread_id&& other) noexcept : _size(other._size), _data(other._data) {
 			other._size = 0;
 		}
 
-		thread_id& operator=(thread_id const& other) noexcept {
-			if(&other == this)
-				return *this;
-
-			resize(other._size);
-
-			const void* from = _size <= asize ? other._data.a : other._data.p;
-			std::memcpy(data(), from, _size * sizeof(*data()));
-
-			return *this;
-		}
-
 		thread_id& operator=(thread_id&& other) noexcept {
 			if(this != &other) {
-				if(_size > asize) delete[] _data.p;
-				std::memcpy(this, &other, sizeof(thread_id));
+				clear();
+				_size = other._size;
+				_data = other._data;
 				other._size = 0;
 			}
 			return *this;
 		}
 
-		~thread_id() noexcept { if(_size > asize) delete[] _data.p; }
+		~thread_id() noexcept { clear(); }
 
-		auto const& size() const noexcept { return _size; }
+		bool empty() const noexcept { return _size == 0; }
+		explicit operator bool() const noexcept { return !empty(); }
+		std::size_t size() const noexcept { return _size; }
 
-		std::uint16_t const* ids() const noexcept { return _size <= asize ? _data.a : _data.p; }
+		std::uint16_t const* ids() const noexcept { return data(); }
 
-		std::uint16_t localId() const noexcept {
-			return _size == 0 ? 0 : ids()[_size - 1];
+		std::uint16_t operator[](std::size_t const index) const noexcept {
+			return data()[index];
 		}
 
-		std::uint16_t operator[](std::size_t const index) noexcept {
-			return ids()[index];
-		}
-
-		std::uint16_t const& operator[](std::size_t const index) const noexcept {
-			return ids()[index];
-		}
-
-		bool empty() noexcept { return _size == 0; };
+		auto begin() noexcept { return data(); }
+		auto begin() const noexcept { return data(); }
+		auto end() noexcept { return data() + size(); }
+		auto end() const noexcept { return data() + size(); }
 
 		friend bool operator==(thread_id const& lhs, thread_id const& rhs) noexcept {
-			return lhs.size() == rhs.size() && std::memcmp(lhs.ids(), rhs.ids(), lhs.size() * sizeof(*lhs.ids())) == 0;
-		}
-
-		friend bool operator<(thread_id const& lhs, thread_id const& rhs) noexcept {
-			return lhs.size() < rhs.size() || std::memcmp(lhs.ids(), rhs.ids(), lhs.size() * sizeof(*lhs.ids())) < 0;
+			return lhs.size() == rhs.size() && std::memcmp(lhs.data(), rhs.data(), lhs.size() * sizeof(*lhs.data())) == 0;
 		}
 
 		friend bool operator!=(thread_id const& lhs, thread_id const& rhs) noexcept {
 			return !(lhs == rhs);
 		}
 
-		explicit operator bool() const noexcept {
-			return _size > 0;
+		friend bool operator<(thread_id const& lhs, thread_id const& rhs) noexcept {
+			for(std::size_t i = 0; i < lhs.size() && i < rhs.size(); ++i) {
+				if(lhs.data()[i] < rhs.data()[i]) {
+					return true;
+				} else if(rhs.data()[i] < lhs.data()[i]) {
+					return false;
+				}
+			}
+			return lhs.size() < rhs.size();
+		}
+
+		friend bool operator>(thread_id const& lhs, thread_id const& rhs) noexcept {
+			for(std::size_t i = 0; i < lhs.size() && i < rhs.size(); ++i) {
+				if(lhs.data()[i] > rhs.data()[i]) {
+					return true;
+				} else if(rhs.data()[i] > lhs.data()[i]) {
+					return false;
+				}
+			}
+			return lhs.size() > rhs.size();
+		}
+
+		friend bool operator<=(thread_id const& lhs, thread_id const& rhs) noexcept {
+			for(std::size_t i = 0; i < lhs.size() && i < rhs.size(); ++i) {
+				if(lhs.data()[i] < rhs.data()[i]) {
+					return true;
+				} else if(rhs.data()[i] < lhs.data()[i]) {
+					return false;
+				}
+			}
+			return lhs.size() <= rhs.size();
+		}
+
+		friend bool operator>=(thread_id const& lhs, thread_id const& rhs) noexcept {
+			for(std::size_t i = 0; i < lhs.size() && i < rhs.size(); ++i) {
+				if(lhs.data()[i] > rhs.data()[i]) {
+					return true;
+				} else if(rhs.data()[i] > lhs.data()[i]) {
+					return false;
+				}
+			}
+			return lhs.size() >= rhs.size();
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, thread_id const& tid) {
+			bool first = true;
+			for(auto entry : tid) {
+				if(!first) {
+					os << ',';
+				}
+
+				os << entry;
+				first = false;
+			}
+			return os;
 		}
 
 		std::string to_string() const {
 			std::ostringstream buffer;
-
-			for(std::size_t i = 0; i < size(); i++) {
-				if(i > 0)
-					buffer << ',';
-
-				buffer << ids()[i];
-			}
-
+			buffer << *this;
 			return buffer.str();
 		}
 
-		static std::optional<thread_id> from_string(std::string tidAsString) {
-			std::istringstream sstream(tidAsString);
+		static std::optional<thread_id> from_string(std::string const& tid_as_string) {
+			std::istringstream sstream(tid_as_string);
 			if (!sstream) {
 				return {};
 			}
@@ -190,9 +223,4 @@ namespace por {
 			return tid;
 		}
 	};
-
-	inline std::ostream &operator<<(std::ostream &os, thread_id const& tid) {
-		os << "tid<" << tid.to_string() << ">";
-		return os;
-	}
 }
