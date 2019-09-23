24f5f358431859b9d182f65784771a46f429e336
David Laprell <david.laprell@rwth-aachen.de>
2019-09-23 10:29:43 +0200

    Remove xfail from now passing test and test the dataraces with different thread schedules

--- test/Runtime/pthread/datarace/concrete.c
+++ test/Runtime/pthread/datarace/concrete.c
@@ -1,7 +1,17 @@
 // RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
-// RUN: rm -rf %t.klee-out
-// RUN: %klee  --pthread-runtime --output-dir=%t.klee-out %t.bc 2>&1
-// RUN: test -f %t.klee-out/test000001.unsafememoryaccess.err
+// RUN: rm -rf %t-first.klee-out
+// RUN: rm -rf %t-last.klee-out
+// RUN: rm -rf %t-random.klee-out
+// RUN: rm -rf %t-round-robin.klee-out
+// RUN: %klee -pthread-runtime -exit-on-error-type=Assert -output-dir=%t-first.klee-out -thread-scheduling=first %t.bc 2>&1
+// RUN: %klee -pthread-runtime -exit-on-error-type=Assert -output-dir=%t-last.klee-out -thread-scheduling=last %t.bc 2>&1
+// RUN: %klee -pthread-runtime -exit-on-error-type=Assert -output-dir=%t-random.klee-out -thread-scheduling=random %t.bc 2>&1
+// RUN: %klee -pthread-runtime -exit-on-error-type=Assert -output-dir=%t-round-robin.klee-out -thread-scheduling=round-robin %t.bc 2>&1
+// RUN: test -f %t-first.klee-out/test000001.unsafememoryaccess.err
+// RUN: test -f %t-last.klee-out/test000001.unsafememoryaccess.err
+// RUN: test -f %t-random.klee-out/test000001.unsafememoryaccess.err
+// RUN: test -f %t-round-robin.klee-out/test000001.unsafememoryaccess.err
+
 
 #include <pthread.h>
 #include <assert.h>
