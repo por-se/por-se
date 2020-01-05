c472f686df9ff67ae82b0166cf2e2f2521208c90
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-01-05 13:26:34 +0100

    disable Realloc.c test with verified fingerprints

--- test/Feature/Realloc.c
+++ test/Feature/Realloc.c
@@ -1,3 +1,4 @@
+// REQUIRES: not-verified-fingerprints
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --exit-on-error --warnings-only-to-file=false %t1.bc 2>&1 | FileCheck %s
