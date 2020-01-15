09461b42d898b35df764064ef67c5a6f2a968b04
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-01-15 19:06:06 +0100

    disable PrgName.c test with verified fingerprints

--- test/Runtime/POSIX/PrgName.c
+++ test/Runtime/POSIX/PrgName.c
@@ -1,3 +1,4 @@
+// REQUIRES: not-verified-fingerprints
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t2.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --posix-runtime --exit-on-error %t2.bc --sym-arg 10 >%t.log
