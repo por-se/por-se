6b4807f2ba8f3679528010fd0350253de152fa4c
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-01-25 20:09:12 +0100

    disable max-csd for five-wakeups.c test

--- test/Runtime/POSIX/pthread/regression/five-wakeups.c
+++ test/Runtime/POSIX/pthread/regression/five-wakeups.c
@@ -1,6 +1,6 @@
 // RUN: %clang %s -emit-llvm %O0opt -g -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --posix-runtime --exit-on-error --explore-schedules=0 --thread-scheduling=last %t.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out --posix-runtime --exit-on-error --explore-schedules=0 --max-csd=0 --thread-scheduling=last %t.bc 2>&1 | FileCheck %s
 
 #include <assert.h>
 #include <pthread.h>
