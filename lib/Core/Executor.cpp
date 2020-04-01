85a94514bacbb4b168ae9ba711caa2234f6220cb
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-04-01 16:20:44 +0200

    fix: actually manipulate the right state

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4465,12 +4465,12 @@ void Executor::executeFree(ExecutionState &state,
 
         const auto& allocatorTid = mo->getAllocationStackFrame().first;
 
-        if (allocatorTid == state.tid()) {
-          state.performAllocatorFree(mo);
+        if (allocatorTid == it->second->tid()) {
+          it->second->performAllocatorFree(mo);
         } else {
           // So this was not allocated by our own thread. Therefore, we want to delay the
           // free to the correct thread
-          state.thread().unsynchronizedFrees[allocatorTid].emplace_back(mo);
+          it->second->thread().unsynchronizedFrees[allocatorTid].emplace_back(mo);
         }
 
         // We want to always unbind the memory object from the addressspace in order to detect
