9c1123118d04f89ce97f109f7fbe17d52cb245c2
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-04-01 16:06:42 +0200

    use synchronized_events() for delayed frees

--- lib/Core/por/DelayedFreesContainer.cpp
+++ lib/Core/por/DelayedFreesContainer.cpp
@@ -1,47 +1,27 @@
 #include "DelayedFreesContainer.h"
 
 #include "por/event/event.h"
-#include "../Memory.h"
-
-#include "cassert"
 
 using namespace klee;
 
 void DelayedFreesContainer::drainFrees(const por::event::event &newEvt, FreeCallback callback) {
   // So we want to find all events (except from events that we triggered) and
-  // check if their are any frees we have to perform
+  // check if there are any frees we have to perform
 
   if (newEvt.kind() == por::event::event_kind::thread_init) {
-    // So we only init the current thread, therefore, their cannot
+    // So we only init the current thread, therefore, there cannot
     // be any memory objects that we had created before
     return;
   }
 
-  auto& checkedThreads = alreadyDone[newEvt.tid()];
-
-  for (const auto& it : newEvt.cone()) {
-    if (it.first == newEvt.tid()) {
-      continue;
-    }
-
-    const auto* checked = checkedThreads[it.first];
-
-    const auto* cur = it.second;
-    for (; cur != nullptr && (checked == nullptr || checked->is_less_than(*cur)); cur = cur->thread_predecessor()) {
-      auto& delayedFreesPerThread = cur->metadata().pending_frees;
-
-      // Check if there is something for this thread
-      auto it = delayedFreesPerThread.find(newEvt.tid());
-      if (it == delayedFreesPerThread.end()) {
-        continue;
-      }
-
+  for (const auto *pred : newEvt.synchronized_events()) {
+    auto& delayedFreesPerThread = pred->metadata().pending_frees;
+    // Check if there is something for this thread
+    auto it = delayedFreesPerThread.find(newEvt.tid());
+    if (it != delayedFreesPerThread.end()) {
       for (const auto* mo : it->second) {
         callback(mo);
       }
     }
-
-    // Make sure that we will not check this event a second time
-    checkedThreads[it.first] = it.second;
   }
 }
--- lib/Core/por/DelayedFreesContainer.h
+++ lib/Core/por/DelayedFreesContainer.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "klee/ThreadId.h"
-
-#include <map>
 #include <functional>
 
 namespace por::event {
@@ -16,11 +13,6 @@ namespace klee {
     public:
       using FreeCallback = std::function<void(const MemoryObject*)>;
 
-    private:
-      // Map that keeps track of which events we already checked
-      std::map<ThreadId, std::map<ThreadId, const por::event::event*>> alreadyDone;
-
-    public:
       DelayedFreesContainer() = default;
 
       void drainFrees(const por::event::event &newEvt, FreeCallback callback);
