3094760ea081c73ef520b3f0ed518daca5be1ca1
Julian Büning <julian.buening@comsys.rwth-aachen.de>
2020-04-09 18:27:42 +0200

    fix Thread copy ctor for unsynchronizedFrees

--- lib/Core/Thread.cpp
+++ lib/Core/Thread.cpp
@@ -83,7 +83,8 @@ Thread::Thread(const Thread &t)
           errnoMo(t.errnoMo),
           pathSincePorLocal(t.pathSincePorLocal),
           spawnedThreads(t.spawnedThreads),
-          fingerprint(t.fingerprint) {
+          fingerprint(t.fingerprint),
+          unsynchronizedFrees(t.unsynchronizedFrees) {
 
   threadHeapAlloc = std::make_unique<pseudoalloc::allocator_t>(*t.threadHeapAlloc);
   threadStackAlloc = std::make_unique<pseudoalloc::stack_allocator_t>(*t.threadStackAlloc);
